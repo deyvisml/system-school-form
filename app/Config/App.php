d56578dca30592658b5eb1753edcd149d9c3b7f8
Deyvis ML <deyvisml2002@gmail.com>
2024-01-14 13:40:20 -0500

    last changes related with adding iiee

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/system-school-form/';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
